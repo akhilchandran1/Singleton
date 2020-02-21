3ac499075ab35a6c76536f5d532b418e6ea6c13d
Akhil Chandran <akhil9216@gmail.com>
2020-02-21 15:39:02 +0000

    Initial commit

---
+++ src/Calendar.java
@@ -0,0 +1,43 @@
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class Calendar {
+
+	
+	// HashMap to save one event per date
+	private Map <Date, String> calendar;
+	
+	// Constructor
+	public Calendar () {
+		
+		calendar = new HashMap<Date, String>();
+		
+	}
+	
+	// Add event on a particular date
+	public void addEvent(Date date, String event) {
+		
+		calendar.put(date, event);
+		
+	}
+	
+	// Check event on a particular date
+	public String getEvent(Date date) {
+		
+		return calendar.get(date);
+	
+	}
+	
+	public String getEvents() {
+		String events = "";
+		
+		for (Entry entry : calendar.entrySet()) {
+		    events += entry.getKey() + " - " + entry.getValue() + "\n";
+		}
+		
+		return events;
+	}
+	
+}
---
+++ src/Week1Main.java
@@ -0,0 +1,40 @@
+import java.util.Date;
+
+public class Week1Main {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Week1Main();
+		
+	}
+	
+	public Week1Main() {
+		
+		// Creating a date object
+		Date d1 = new Date(2019,02,04);
+		
+		// Creating a calendar object
+		Calendar calendar = new Calendar();
+		
+		// Adding an event to the calendar
+		calendar.addEvent(d1, "Amilcar's Class");
+		
+		// Retrieving the event on the date
+		System.out.println(calendar.getEvent(d1));
+		
+		// Creating a date object
+		Date d2 = new Date(2019,02,05);
+				
+		// Adding another event to the calendar
+		calendar.addEvent(d2, "Greg's Class");
+		
+		System.out.println("Generic Calendar");
+		// Retrieving all events
+		System.out.println(calendar.getEvents());
+		
+		
+	
+	}
+
+}
